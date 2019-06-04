ab6a640ac6a0dce3013a441a885ce1c6fedd3de4
baron <owenashby@hotmail.nl>
2019-06-04 15:59:43 +0200

    made cubes clickable

---
+++ Assets/Scripts/ShelfManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShelfManager : MonoBehaviour {
+
+    [SerializeField] int shelfId;
+    [SerializeField] int booksOnShelf;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed on shelf with ID: " + shelfId);
+            GameObject parent = transform.parent.gameObject;
+            parent.SetActive(false);
+        }
+    }
+}
